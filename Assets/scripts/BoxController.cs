3c8dce201701dfdd390f0a6625c8ff2274b9f17c
Yuanguo-notebook <45431499+Yuanguo-notebook@users.noreply.github.com>
2019-09-07 19:40:13 -0400

    Add files via upload

---
+++ Assets/scripts/BoxController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour {
+	Texture2D tex;
+	void Start(){
+		tex = Texture2D.whiteTexture;
+	}
+	public void generate(int row, int col){
+		
+		float width = 2.5f;
+		float xStart = -col * width / 2f + width;
+		float yStart = width / 2f +3;
+
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+				box.transform.position = new Vector3 (xStart + i * width / 2f, yStart + j * width / 2f, width);
+				box.AddComponent<Rigidbody> ();
+				box.AddComponent<BoxCollider> ();
+
+				Renderer rend = box.GetComponent<Renderer> ();
+				rend.material.mainTexture = tex;
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Control.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	public BoxController boxController;
+
+
+	public int row;
+	public int col;
+	public int power = 5;
+
+	// Use this for initialization
+	void Start () {
+		boxController = new BoxController ();
+		boxController.generate (row, col);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray,out hit)){
+				ball (hit.point);
+			}
+		}
+	}
+
+	void ball(Vector3 vec){
+		GameObject ball = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		ball.transform.position = Camera.main.transform.position;
+		ball.AddComponent<BoxCollider> ();
+		Rigidbody rb = ball.AddComponent<Rigidbody> ();
+		rb.AddForce (power*(vec - ball.transform.position), ForceMode.Impulse); 
+	}
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
